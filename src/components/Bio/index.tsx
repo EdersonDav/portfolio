6948fb8047cea05b15c3759ba868c91384cba78f
EdersonDav <ederson.silva@sunoresearch.com.br>
2022-03-19 16:59:01 -0300

    feat: add bio informations

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -1,21 +1,48 @@
+import {useState, useEffect} from 'react';
 import { User } from '../../types/interfaces';
-import { Container, BioText, ImagesBio } from './style';
+import { Container, BioText, ImagesBio, Skkils } from './style';
 
 interface BioProps {
   informations: User;
 }
 
 export const Bio = ({ informations }: BioProps) => {
+  const [skills, setSkills] = useState<string[]>([]);
+  const [bioTitle, setBioTitle] = useState('');
+  const [bioSubTitle, setBioSubTitle] = useState('');
+
+  useEffect(()=>{
+    const bioGit = informations.bio;
+
+    setBioTitle(bioGit.split('.')[0])
+    setBioSubTitle(bioGit.split('.')[1].split(':')[0])
+    const skillsText = bioGit.split(':')[1].split(',')
+    setSkills(skillsText)
+    
+  }, []);
+
   return (
     <Container>
       <BioText>
-        <span>Full-Stack Developer</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore minima
-          sunt at, cumque quas, facere libero ab vero temporibus facilis
-          adipisci. Suscipit, at. Ipsam ex, nobis deleniti pariatur maxime
-          alias.
-        </p>
+        <div>{bioTitle}</div>
+        <div>
+          <p>
+            Hello, I'm Ederson Davi!
+          </p>
+          <Skkils>
+            <p>{bioSubTitle}</p>
+            <ul>
+              {skills?.length ? (
+                <>
+                  {skills.map(sk =>(
+                    <li key={sk}>- {' '} {sk.trim()}</li>
+                  ))}
+                </>
+              ): null}
+            </ul>
+          </Skkils>
+        </div>
+        
       </BioText>
       <ImagesBio>
         <img
--- src/components/Bio/style.ts
+++ src/components/Bio/style.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: flex-start;
-  margin-top: 200px;
+  margin-top: 150px;
   justify-content: space-around;
   padding: 0 200px;
 `
@@ -12,23 +12,38 @@ export const Container = styled.div`
 export const BioText = styled.div`
   display: flex;
   flex-direction: column;
-  >span{
+  padding-right: 100px;
+  >div:first-child{
     margin-bottom: 50px;
     font-size: 24px;
     background-color: var(--purple);
     padding: 10px;
-    max-width: 300px;
     font-weight: 500;
     display: flex;
     justify-content: center;
     color: var(--white);
     border-radius: 20px;
+    max-width: 500px;
   }
-  >p{
+  p{
     color: var(--white);
     font-size: 24px;
-    max-width: 80%;
     line-height: 30px;
+    margin-bottom: 10px;
+  }
+`
+
+export const Skkils = styled.div`
+  p{
+    color: var(--white);
+    font-size: 20px;
+    line-height: 30px;
+    margin-bottom: 10px;
+  }
+  ul{
+    color: var(--white);
+    font-size: 18px;
+    line-height: 25px;
   }
 `
 
