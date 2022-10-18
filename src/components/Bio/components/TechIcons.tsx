9f7240cec3c503a30b3e4b010699aab8857b3808
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-10-18 07:07:15 -0300

    feat(style): style techs icons

--- src/components/Bio/components/TechIcons.tsx
+++ src/components/Bio/components/TechIcons.tsx
@@ -2,27 +2,31 @@ import { SiTypescript, SiNextdotjs, SiPostgresql } from 'react-icons/si';
 import { IoLogoNodejs } from 'react-icons/io';
 import { FaReact } from 'react-icons/fa';
 
-import { Container } from './style';
+import { Container, IconContainer } from './style';
 
 export const TechIcons = ():JSX.Element =>{
 
   return(
     <Container>
-      <div>
-        <SiTypescript/>
-      </div>
-      <div>
+      <IconContainer color='#2D79C7'>
+        <SiTypescript color='#2D79C7'/>
+      </IconContainer>
+
+      <IconContainer color='#aaa'>
         <SiNextdotjs/>
-      </div>
-      <div>
-        <SiPostgresql/>
-      </div>
-      <div>
-        <IoLogoNodejs/>
-      </div>
-      <div>
-        <FaReact/>
-      </div>
+      </IconContainer>
+
+      <IconContainer color='#2D79C7'>
+        <SiPostgresql color='#2F6792'/>
+      </IconContainer>
+
+      <IconContainer color='#5E955B'>
+        <IoLogoNodejs color='#5E955B'/>
+      </IconContainer>
+
+      <IconContainer color='#2D79C7'>
+        <FaReact color='#54C1D8'/>
+      </IconContainer>
     </Container>
   )
 
--- src/components/Bio/components/style.ts
+++ src/components/Bio/components/style.ts
@@ -7,15 +7,156 @@ export const Container = styled.div`
   width:100%;
   height:100%;
   >div{
-    width: 100px;
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    background-color: var(--white);
-    >svg{
-      font-size: 50px;
+    
+  }
+`
+
+export const IconContainer = styled.div<{color: string}>`
+  width: 22%;
+  height: 22%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: var(--white);
+  position: absolute;
+  box-shadow: -1px 10px 30px -5px ${({color})=> color ? color : 'var(--white)'};
+  >svg{
+    font-size: 50px;
+  }
+  &:nth-child(1){
+    top: 4%;
+    right: 2%;
+  }
+  &:nth-child(2){
+    bottom: -10%;
+    right: 10%;
+    width: 15%;
+    height: 15%;
+    svg{
+      font-size: 40px;
+    }
+  }
+
+  &:nth-child(3){
+    bottom: 40%;
+    right: 10%;
+    width: 12%;
+    height: 12%;
+    svg{
+      font-size: 30px;
+    }
+  }
+
+  &:nth-child(4){
+    bottom: 10%;
+    left: -5%;
+  }
+
+  &:nth-child(5){
+    top: 20%;
+    left: 8%;
+    width: 15%;
+    height: 15%;
+  }
+
+  @media(max-width: 1080px){
+    width: 17%;
+    height: 20%;
+    svg{
+      font-size: 40px;
+    }
+
+    &:nth-child(1){
+      right: -20%;
+    }
+    &:nth-child(2){
+      width: 12%;
+      height: 14%;
+      bottom: 32%;
+      right: -47%;
+      svg {
+        font-size: 30px;
+      }
+    }
+    &:nth-child(4){
+      left: 52%;
+    }
+    
+    &:nth-child(3){
+      width: 9%;
+      height: 11%;
+      svg{
+        font-size: 22px;
+      }
+    }
+
+    &:nth-child(5){
+      width: 12%;
+      left: 63%;
+    }
+  }
+
+  @media(max-width: 760px){
+    width: 12%;
+    height: 12%;
+    svg{
+      font-size: 28px;
+    }
+
+    &:nth-child(1){
+      top: -3%;
+      left: 2%;
+    }
+    &:nth-child(2){
+      width: 10%;
+      height: 11%;
+      bottom: 59%;
+      left: 1%;
+      svg {
+        font-size: 28px;
+      }
+    }
+    &:nth-child(4){
+      bottom: 40%;
+    }
+    
+    &:nth-child(3){
+      height: 10%;
+      bottom: 27%;
+      left: 6%;
+    }
+
+    &:nth-child(5){
+      width: 10%;
+      height: 11%;
+    }
+  }
+
+  @media(max-width: 450px){
+    width: 12%;
+    height: 12%;
+    svg{
+      font-size: 22px;
+    }
+    &:nth-child(2){
+      height: 10%;
+      svg {
+        font-size: 20px;
+      }
+    }
+    
+    &:nth-child(3){
+      width: 10%;
+      height: 10%;
+      bottom: 35%;
+      svg {
+        font-size: 20px;
+      }
+    }
+
+    &:nth-child(5){
+      height: 10%;
     }
   }
 `
